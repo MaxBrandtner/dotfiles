6a0db4801ad83e172781c87cd1f372d26b5251c5
MaxBrandtner <maximilian.immanuel.brandtner@protonmail.com>
2024-07-22 17:19:28 +0200

    adds options to install script

--- install.sh
+++ install.sh
@@ -2,10 +2,33 @@
 initial_dir=$(pwd)
 cd "$(dirname "$(realpath "$BASH_SOURCE")")"
 
-cp -R .config/ $HOME
-cp .tmux.conf $HOME
-cp .zshrc $HOME
+if [ -z "$1" ]
+then
+	echo "Usage: ./install.sh [...]"   >/dev/stderr
+	echo "	--conf	Update dotfiles"   >/dev/stderr
+	echo "	--nix	Update nix-config" >/dev/stderr
+	exit 1
+fi
 
-tmux source $HOME/.tmux.conf >/dev/null 2>&1
+if [ "$1" == "--conf" ]
+then
+	cp -R .config/ $HOME
+	cp .tmux.conf $HOME
+	cp .zshrc $HOME
+
+	tmux source $HOME/.tmux.conf >/dev/null 2>&1
+
+fi
+
+if [ "$1" == "--nix" ]
+then
+	if [ "$(whomai)" != "root" ]
+	then
+		echo "Must be run as root to set nix config" >/dev/stderr
+		exit 1
+	fi
+	cp nixos/configuration.nix /etc/nixos/configuration.nix
+	nixos-rebuild switch
+fi
 
 cd "$initial_dir"

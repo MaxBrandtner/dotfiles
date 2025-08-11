1b42c9ec710d7ad33ba8f6895eaff9291fb84460
Maximilian Immanuel Brandtner <maxbr@linux.ibm.com>
2025-08-11 10:45:05 +0200

    New NVIM config
    Signed-off-by: Maximilian Immanuel Brandtner <maxbr@linux.ibm.com>

---
+++ .config/nvim/lua/lsp.lua
@@ -0,0 +1,78 @@
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+local lspconfig = require('lspconfig')
+
+-- name: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+-- executable: name of the language server executable in $PATH
+local servers = {
+	{ name = 'clangd',        executable = 'clangd' },
+	{ name = 'rust_analyzer', executable = 'rust-analyzer' },
+	{ name = 'pyright',       executable = 'pyright' },
+	{ name = 'lua_ls',        executable = 'lua-language-server' },
+	{ name = 'bashls',        executable = 'bash-language-server' },
+}
+
+-- only set up LSP if it exists in $PATH
+for _, server in ipairs(servers) do
+	if Is_executable_in_path(server.executable) then
+		if server.name == "rust_analyzer" then
+			lspconfig[server.name].setup {
+				capabilities = capabilities,
+				settings = {
+					["rust-analyzer"] = {
+						check = {
+							command = "clippy",
+						},
+					},
+				},
+			}
+		elseif server.name == "bashls" then
+			lspconfig[server.name].setup {
+				capabilities = capabilities,
+				filetypes = { "sh", "bash", "zsh" },
+			}
+		else
+			lspconfig[server.name].setup {
+				capabilities = capabilities,
+			}
+		end
+	end
+end
+
+-- nvim-cmp: completion engine plugin for neovim
+local cmp = require 'cmp'
+cmp.setup {
+	snippet = {
+		expand = function(args)
+			vim.snippet.expand(args.body)
+		end,
+	},
+	mapping = cmp.mapping.preset.insert({
+		['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
+		['<C-d>'] = cmp.mapping.scroll_docs(4), -- Down
+		-- C-b (back) C-f (forward) for snippet placeholder navigation.
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<CR>'] = cmp.mapping.confirm {
+			behavior = cmp.ConfirmBehavior.Replace,
+			select = true,
+		},
+		["TAB"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			else
+				fallback()
+			end
+		end, { 'i', 's' }),
+		-- Shift + Tab
+		["S-TAB"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			else
+				fallback()
+			end
+		end, { 'i', 's' }),
+	}),
+	sources = {
+		{ name = 'nvim_lsp' },
+	},
+}
---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,34 @@
+-- Automatically install vimplug
+local data_dir = vim.fn.stdpath('data')
+if vim.fn.empty(vim.fn.glob(data_dir .. '/site/autoload/plug.vim')) == 1 then
+	vim.cmd('silent !curl -fLo ' ..
+		data_dir ..
+		'/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim')
+	vim.o.runtimepath = vim.o.runtimepath
+	vim.cmd('autocmd VimEnter * PlugInstall --sync | source $MYVIMRC')
+end
+
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin')
+-- vim plugins
+Plug 'tpope/vim-fugitive'
+Plug 'tpope/vim-obsession'
+Plug 'takac/vim-hardtime'
+Plug 'rhysd/vim-clang-format'
+-- Plug 'lervag/vimtex'
+-- neovim plugins
+Plug 'neovim/nvim-lspconfig'
+Plug 'hrsh7th/nvim-cmp'
+Plug 'hrsh7th/cmp-nvim-lsp'
+Plug 'j-hui/fidget.nvim'
+Plug 'nvim-lualine/lualine.nvim'
+Plug('ibhagwan/fzf-lua', { branch = 'main' })
+Plug 'ggandor/leap.nvim'
+-- Plug 'stevearc/oil.nvim'
+Plug('catppuccin/nvim', { as =  'catppuccin' })
+Plug 'lewis6991/gitsigns.nvim'
+Plug 'kdheepak/lazygit.nvim'
+-- Plug('iamcco/markdown-preview.nvim', { ['do'] = 'cd app && yarn install', ['for'] = { 'markdown' } })
+
+vim.call('plug#end')

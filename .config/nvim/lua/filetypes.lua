10583ef34a12814bb3b9d890901cbf10cb5fe970
Max Brandtner <maximilian.immanuel.brandtner@protonmail.com>
2025-08-14 11:31:47 +0200

    add machine description filetype
    Signed-off-by: Max Brandtner <maximilian.immanuel.brandtner@protonmail.com>

--- .config/nvim/lua/filetypes.lua
+++ .config/nvim/lua/filetypes.lua
@@ -7,3 +7,13 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.opt_local.softtabstop = 0
     end,
 })
+
+vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
+    pattern = "gcc/*/*.md",
+    callback = function
+	vim.opt_local.expandtab = false
+	vim.opt_local.shiftwidth = 4
+	vim.opt_local.tabstop = 4
+	vim.opt_lcoal.softtabstop = 0
+    end,
+})

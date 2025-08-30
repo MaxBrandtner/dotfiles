66056b4bc2eb661e62ef8f720f125f21c0f59468
Max Brandtner <maximilian.immanuel.brandtner@protonmail.com>
2025-08-30 12:14:02 +0200

    add htmldjango format
    Signed-off-by: Max Brandtner <maximilian.immanuel.brandtner@protonmail.com>

--- .config/nvim/lua/filetypes.lua
+++ .config/nvim/lua/filetypes.lua
@@ -49,16 +49,7 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = {"html"},
-	callback = function()
-		vim.opt_local.expandtab = true
-		vim.opt_local.shiftwidth = 2
-		vim.opt_local.tabstop = 2
-	end,
-})
-
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = {"xml"},
+	pattern = {"html", "htmldjango", "xml"},
 	callback = function()
 		vim.opt_local.expandtab = true
 		vim.opt_local.shiftwidth = 2
